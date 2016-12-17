8948e45810542aed506195edfff486f4409a2748
Artem L <artuon@gmail.com>
2016-12-17 09:36:20 +0200

    Rectification of Drush Sync

--- Drupal/Drush/sync_db_and_files.sh
+++ Drupal/Drush/sync_db_and_files.sh
@@ -1,7 +1,7 @@
 #You need to setup Drush aliases in advance
 
 #Sync DB (live to stage)
-drush sql-sync @live @stage
+drush sql-sync @source_alias @target_alias
 
 #Sync files (live to stage)
-drush rsync @live:%files @stage:%files
+drush rsync @source_alias:%files @target_alias:%files
