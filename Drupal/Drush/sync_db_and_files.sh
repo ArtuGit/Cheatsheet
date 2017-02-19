f676b1b0074e121368ebc639b739b026e0d06963
Artem L <artuon@gmail.com>
2017-02-19 15:09:19 +0200

    Update drush sync

--- Drupal/Drush/sync_db_and_files.sh
+++ Drupal/Drush/sync_db_and_files.sh
@@ -4,4 +4,4 @@
 drush sql-sync @source_alias @target_alias
 
 #Sync files (live to stage)
-drush rsync @source_alias:%files @target_alias:%files
+drush rsync @source_alias:%files/ @target_alias:%files
