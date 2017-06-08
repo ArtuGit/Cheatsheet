0ee26b3fe96567e94c089dc4c45e313fcf647b8b
Artem L <artuon@gmail.com>
2017-06-08 11:04:21 +0300

    Drushb user's password

--- Drupal/Drush/drush.sh
+++ Drupal/Drush/drush.sh
@@ -1,2 +1,10 @@
 #Clean 'flood' table
 drush php-eval 'db_query("DELETE FROM flood");'
+
+#Change user's password
+drush user-password USERNAME --password="SOMEPASSWORD"
+or abbreviated
+drush upwd USERNAME --password="SOMEPASSWORD"
+
+#Reset user's password
+drush uli some-username
