97b9104d4ca36aee9a2714fd6373d4849d4e6079
Unknown <artuon@gmail.com>
2018-05-31 22:41:07 +0300

    Plasma and others

---
+++ Linux /Debian/Plasma/plasma-restart.sh
@@ -0,0 +1,5 @@
+kquitapp5 plasmashell > /dev/null 2>&1 &
+kstart plasmashell > /dev/null 2>&1 &
+ibus-daemon &
+killall -9 Viber
+/opt/viber/Viber /dev/null 2>&1 &
--- Linux /Debian/Plasma/plasma.md
+++ Linux /Debian/Plasma/plasma.md
@@ -1,2 +1,6 @@
 ### Restart IBus
 ibus-daemon &
+
+### Restart Plasma
+kquitapp5 plasmashell && kstart plasmashell
+https://www.lifewire.com/kubuntu-p2-2202573
