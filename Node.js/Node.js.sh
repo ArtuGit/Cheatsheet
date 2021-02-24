1fbec9edfe824f13cba246a89870e4922cd1fbbd
Artem L <artuon@gmail.com>
2021-02-24 15:12:41 +0200

    Find node proces on a specific port

--- Node.js/Node.js.sh
+++ Node.js/Node.js.sh
@@ -1,9 +1,12 @@
-#How to see all node process
+# How to see all node process
 ps aux | grep node
 
-#Kill ths specific node process
+# Proces on a specific port
+lsof -i tcp:3000
+
+# Kill ths specific node process
 kill -9 PROCESS_ID
 
-#Kill all node process. Be sure!
+# Kill all node process. Be sure!
 killall node
 
