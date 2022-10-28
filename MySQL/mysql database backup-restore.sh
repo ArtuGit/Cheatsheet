b8cf1b81f716f9b02165af938b1eef4809fdd566
Artem L <artuon@gmail.com>
2022-10-28 10:33:04 +0300

    Mysql Docker

--- MySQL/mysql database backup-restore.sh
+++ MySQL/mysql database backup-restore.sh
@@ -1,4 +1,16 @@
-#Backup
+## Local
+
+###Backup
 mysqldump -u [user_name] -p [db_name] > ~/dumpname_yymmdd.sql
-#Restore
+
+###Restore
 mysql -u [user_name] -p [db_name] < ~/dumpname_yymmdd.sql
+
+
+# Docker
+
+###Backup
+docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
+
+###Restore
+cat backup.sql | docker exec -i CONTAINER /usr/bin/mysql -u root --password=root DATABASE
